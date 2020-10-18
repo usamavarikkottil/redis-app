5a745acc6b90b251544176919a3337390a236523
usama v <varikkottilusama@gmail.com>
2020-10-18 16:15:18 +0530

    user search done

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require("path");
 const redis = require("redis");
 const methodOverride = require("method-override");
 
+// Create redis client
+let client = redis.createClient();
+client.on("connect", () => {
+    console.log("connected to redis...");
+})
 // Setting port
 const port = 5000;
 
@@ -20,9 +25,29 @@ app.use(express.urlencoded({extended:false}));
 app.use(methodOverride("_method"));
 
 // Routes
+
+// Search page
 app.get("/", (req, res, next) => {
 res.render("searchusers");
 });
 
+
+// Search processs
+app.post("/users/search", (req, res, next) => {
+
+    let id = req.body.id;
+    client.hgetall(id, function (err, obj) {
+        
+        if (!obj) {
+            res.render("searchusers", { error: "user does not exists..."});
+            
+        } else {
+            obj.id = id;
+            res.render("details", {user: obj})
+            
+        }
+    })
+
+})
 
 app.listen(port, () => console.log(`Server Started on port ${port}`));
